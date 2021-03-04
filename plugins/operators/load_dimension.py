47e023bb25b469905b28729e4b256c8b12b2d255
mznamer <mznamer@gmail.com>
2021-03-03 20:26:35 -0800

    load dimension operator fixes

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -10,7 +10,6 @@ class LoadDimensionOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
-                 aws_credentials_id="",
                  sql_query="",
                  destination_table=""
                  *args, **kwargs):
@@ -23,15 +22,17 @@ class LoadDimensionOperator(BaseOperator):
 
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator is started')
+
+        self.log.info(f"LoadDimensionOperator for {self.destination_table} is started")
 
         redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
         self.log.info(f"Clearing data from {self.destination_table} Redshift table")
-        sql_stmt = self.sql_delete.format(self.self.destination_table)
+        sql_stmt = self.sql_delete.format(self.destination_table)
         redshift.run(sql_stmt)
 
         self.log.info(f"Inserting data in {self.destination_table}")
         sql_stmt = self.sql_insert.format(self.destination_table, self.sql_query)
         redshift.run(sql_stmt)
-        
+
+        self.log.info(f"LoadDimensionOperator for {self.destination_table} is finished")
